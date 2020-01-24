fa913031453bbee1364c2ab3f714e9db72974b27
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-01-24 22:04:35 +0000

    + last rendering steps

--- sorting/bubble/js/bubble.js
+++ sorting/bubble/js/bubble.js
@@ -3,7 +3,7 @@ import {DeltaTime, deltaTimeEvent} from '/assets/js/modules/deltaTime.js'
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
-const dt = new DeltaTime(100, animCallback);
+const dt = new DeltaTime(1, animCallback);
 
 dt.start();
 
@@ -17,7 +17,7 @@ renderAllLines();
 
 animate();
 
-renderAllLines();
+highlightLines(lines, 'green');
 
 async function animate() {
     let sorted = false;
@@ -44,12 +44,26 @@ async function animate() {
         ctx.strokeStyle = 'black';
 
         if(!sorted) {
+            let swappedLines = [];
             for(let i = 0; i < lines.length; i++) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                renderAllLines();
+                highlightLines(swappedLines);
+                ctx.strokeStyle = 'red';
+                ctx.beginPath()
+                ctx.moveTo(i, canvas.height - lines[i]);
+                ctx.lineTo(i, canvas.height);
+                ctx.stroke();
+                ctx.strokeStyle = 'black';
+
                 if(lines[i] > lines[i + 1]) {
                     let swap = lines[i + 1];
                     lines[i + 1] = lines[i];
                     lines[i] = swap;
+                    swappedLines.push(lines[i]);
                 }
+
+                await listnerPromise();
             }
         }
     }
@@ -62,6 +76,17 @@ function renderAllLines() {
         ctx.lineTo(i, canvas.height);
         ctx.stroke();
     });
+}
+
+function highlightLines(ar, color) {
+    ctx.strokeStyle = color !== undefined ? color : 'blue';
+    ar.map((line, i) => {
+        ctx.beginPath();
+        ctx.moveTo(i, canvas.height - line);
+        ctx.lineTo(i, canvas.height);
+        ctx.stroke();
+    });
+    ctx.strokeStyle = 'black';
 }
 
 function listnerPromise() {
