9494a42225ae98d770077a5d21e1f7b8f455d631
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-01-24 21:34:16 +0000

    + line generation

--- sorting/bubble/js/bubble.js
+++ sorting/bubble/js/bubble.js
@@ -1,8 +1,27 @@
-import {deltaTime} from '/assets/js/modules/deltaTime.js'
+import {DeltaTime, deltaTimeEvent} from '/assets/js/modules/deltaTime.js'
 
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
-// const dt = new deltaTime(1000, () => console.log('fired'));
+const dt = new DeltaTime(1000, animCallback);
 
-// dt.start();
+dt.start();
+
+canvas.addEventListener('deltaTime', () => console.log('fired'));
+
+const lines = [];
+
+for(let i = 0; i < canvas.width; i++) {
+    lines.push(canvas.height * Math.random());
+}
+
+lines.map((line, i) => {
+    ctx.beginPath();
+    ctx.moveTo(i, canvas.height - line);
+    ctx.lineTo(i, canvas.height);
+    ctx.stroke();
+});
+
+function animCallback() {
+    canvas.dispatchEvent(deltaTimeEvent);
+}
