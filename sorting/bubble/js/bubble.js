5a36cad88e93002d25922bdf9d069a6ccdb37eae
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-01-24 21:51:04 +0000

    + sorting function + some animation code

--- sorting/bubble/js/bubble.js
+++ sorting/bubble/js/bubble.js
@@ -7,8 +7,6 @@ const dt = new DeltaTime(1000, animCallback);
 
 dt.start();
 
-canvas.addEventListener('deltaTime', () => console.log('fired'));
-
 const lines = [];
 
 for(let i = 0; i < canvas.width; i++) {
@@ -21,6 +19,50 @@ lines.map((line, i) => {
     ctx.lineTo(i, canvas.height);
     ctx.stroke();
 });
+
+animate();
+
+lines.map((line, i) => {
+    ctx.beginPath();
+    ctx.moveTo(i, canvas.height - line);
+    ctx.lineTo(i, canvas.height);
+    ctx.stroke();
+});
+
+async function animate() {
+    let sorted = false;
+    while(!sorted) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        sorted = true;
+        for(let i = 0; i < lines.length; i++) {
+            ctx.beginPath()
+            ctx.moveTo(i, canvas.height - lines[i]);
+            ctx.lineTo(i, canvas.height);
+            if(lines[i] > lines[i + 1]) {
+                ctx.strokeStyle = 'red';
+                sorted = false;
+                break;
+            }
+            else ctx.strokeStyle = 'green';
+
+            ctx.stroke()
+
+            await listnerPromise();
+
+            ctx.strokeStyle = 'black';
+        }
+
+        if(!sorted) {
+            for(let i = 0; i < lines.length; i++) {
+                if(lines[i] > lines[i + 1]) {
+                    let swap = lines[i + 1];
+                    lines[i + 1] = lines[i];
+                    lines[i] = swap;
+                }
+            }
+        }
+    }
+}
 
 function listnerPromise() {
     return new Promise((res, reg) => {
