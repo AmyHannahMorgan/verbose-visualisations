0ca9e3c65cff11f97379eb247b377cb6ddd242d4
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-01-24 21:16:36 +0000

    imported deltaTime class

--- sorting/bubble/js/bubble.js
+++ sorting/bubble/js/bubble.js
@@ -1,2 +1,8 @@
+import {deltaTime} from '/assets/js/modules/deltaTime.js'
+
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
+
+const dt = new deltaTime(1000, () => console.log('fired'));
+
+dt.start();
