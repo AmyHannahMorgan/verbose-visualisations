89ab39d2f6314f5bfd6aeecb6ca561a03c225182
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-01-24 21:20:50 +0000

    - deltaTime test object

--- sorting/bubble/js/bubble.js
+++ sorting/bubble/js/bubble.js
@@ -3,6 +3,6 @@ import {deltaTime} from '/assets/js/modules/deltaTime.js'
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
-const dt = new deltaTime(1000, () => console.log('fired'));
+// const dt = new deltaTime(1000, () => console.log('fired'));
 
-dt.start();
+// dt.start();
